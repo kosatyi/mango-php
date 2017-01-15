e087d77fa2e5f58699435b326a7fe33926b86620
Stepan Kosatyi <stepan@kosatyi.com>
2017-01-15 15:48:23 +0200

    start project

--- src/autoload.php
+++ src/autoload.php
@@ -1,10 +1,10 @@
 <?php
 
-function lib_autoload($className){
+function mango_class_autoloader($className){
     $classPath = explode('_',$className);
     $filePath = dirname(__FILE__).'/'.implode('/', $classPath).'.php';
     if(file_exists($filePath))
         require_once( $filePath );
 }
 
-spl_autoload_register('lib_autoload');
+spl_autoload_register('mango_class_autoloader');
